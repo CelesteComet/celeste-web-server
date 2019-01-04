aa8c7c679d9334d561a35a9dd5a13eb63d3ba812
Bruce Wong <brucewong21@gmail.com>
2019-01-03 19:21:01 -0800

    create react frontend skeleton

---
+++ client/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class LikeButton extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { liked: false };
+	}
+
+	render() {
+		if (this.state.liked) {
+			return 'You liked this.';
+		}
+
+		return (
+			<button onClick={() => this.setState({liked: true})}>Like</button>
+		);
+	}
+}
+
+let domContainer = document.querySelector('#app');
+ReactDOM.render(<LikeButton />, domContainer);
--- main.go
+++ main.go
@@ -11,8 +11,10 @@ func SayHello() string  {
 func main() {
 	mux := http.NewServeMux()
 	files := http.FileServer(http.Dir("public/"))
+	index := http.FileServer(http.Dir("client/dist/"))
 
 	mux.Handle("/static/", http.StripPrefix("/static/", files))
+	mux.Handle("/", index)
 
 	server := &http.Server{
 		Addr:			"0.0.0.0:8080",
