85ee420cb1d55883797a3b350700ec1591b8aba6
Bruce Wong <brucewong21@gmail.com>
2019-02-01 20:46:17 -0800

    restructure webpack

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -3,16 +3,18 @@ import axios from 'axios';
 export const RECEIVE_BAGS = 'RECEIVE_BAGS';
 export const FETCH_BAGS ='FETCH_BAGS'
 
-export const receiveBags = bags => ({
-  type: RECEIVE_BAGS,
-  payload: bags 
-})
+export const receiveBags = bags => {
+  return {
+    type: RECEIVE_BAGS,
+    payload: bags
+  }
+}
+
 
 export const fetchBags = () => {
   return dispatch => {
     return axios.get('/api/bags')
       .then(res => {
-        console.log("SHOULDA DISPATCHED")
         dispatch(receiveBags(res.data));
       })
   }
---
+++ client/webpack.dev.js
@@ -0,0 +1,17 @@
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {  
+    contentBase: './dist',
+    historyApiFallback: true,// Fall back to index.html in case file in server can't be found
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        secure: false
+      }
+    }    
+  }
+});
